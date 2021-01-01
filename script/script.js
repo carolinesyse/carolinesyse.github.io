66ee95312785b3d53d9d28cb9899a27bc8c8ea05
Caro <39488494+carolinesyse@users.noreply.github.com>
2021-01-01 18:07:37 +0100

    Henrik <3

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
-console.log("Hei caro");
+// Smooth scroll til About-seksjonen
+document.querySelectorAll('#nav a[href^="#"]').forEach(trigger => {
+    trigger.onclick = function(e) {
+        e.preventDefault();
+        let hash = this.getAttribute('href');
+        let target = document.querySelector(hash);
+        let headerOffset = 50;
+        let elementPosition = target.offsetTop;
+        let offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    };
+});
